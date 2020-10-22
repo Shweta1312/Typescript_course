a0f41e927043ff9fbe864b21231572a0393ae778
Shweta1312 <shawant11@gmail.com>
2020-10-22 11:03:46 +0530

    Array type annotations & inference

---
+++ arrays.ts
@@ -0,0 +1,15 @@
+const days = ["Sunday", "Nothing", "Holiday"];
+
+const nums: number[] = [];
+
+const matrix: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+let var1 = days[0];
+
+days.push("12");
+
+const info: (string | number)[] = ["Its me", 101];
