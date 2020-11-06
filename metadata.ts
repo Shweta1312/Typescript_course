f7a95531ed813d771946fcef96f0dfdc05c2efce
Shweta1312 <shawant11@gmail.com>
2020-11-06 19:53:23 +0530

    Using classes with metadata

--- metadata.ts
+++ metadata.ts
@@ -1,12 +1,18 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red',
-};
-Reflect.defineMetadata('note','hi there',plane,'color');
+class Plane {
+  color: string = 'red';
 
-const note = Reflect.getMetadata('note',plane,'color');
+  @markFunction
+  fly(): void {
+    console.log('vrrrr');
+  }
+}
 
-console.log(note);
-console.log(plane);
+function markFunction(target: Plane, key: string) {
+  Reflect.defineMetadata('secret', 123, target, key);
+}
 
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+console.log(secret);
