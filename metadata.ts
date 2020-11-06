2aadd01510e0d3993f5e63d459c6e46fc6adb9e3
Shweta1312 <shawant11@gmail.com>
2020-11-06 20:09:51 +0530

    Some changes to print the req. metadata from others

--- metadata.ts
+++ metadata.ts
@@ -1,18 +1,26 @@
 import 'reflect-metadata';
 
+@printMetadata
 class Plane {
   color: string = 'red';
 
-  @markFunction
+  @markFunction('Hiii there!!')
   fly(): void {
     console.log('vrrrr');
   }
 }
 
-function markFunction(target: Plane, key: string) {
-  Reflect.defineMetadata('secret', 123, target, key);
+function markFunction(secretInfo: string){
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  }
 }
 
-const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
-
-console.log(secret);
+//Constructor as a parameter
+function printMetadata(target: typeof Plane){
+  for(let key in target.prototype){
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+    
+  }
+}
