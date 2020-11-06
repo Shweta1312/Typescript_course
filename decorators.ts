321e3d0e19c697ee283b14c037a0f65d1929fea3
Shweta1312 <shawant11@gmail.com>
2020-11-06 16:31:42 +0530

    decorator example on 3rd parameter(PropertyDescriptor)

--- decorators.ts
+++ decorators.ts
@@ -1,12 +1,13 @@
 class Boat {
   color: string = 'red';
 
-  @testDecorator
   get formattedColor(): string {
     return `This boat's color is ${this.color}`;
   }
 
+  @logError
   pilot(): void {
+    throw new Error();
     console.log('Swish');
   }
 
@@ -15,7 +16,16 @@ class Boat {
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target: ', target);
-  console.log('Key: ', key);
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log('Oops, the boat was sunk!');
+    }
+  };
 }
+
+new Boat().pilot();
